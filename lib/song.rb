66e5be6b45dcc003d61312e5dd0e4cda11d304e3
Karen V <karenvo@usc.edu>
2019-04-20 10:21:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,8 +24,8 @@ class Song
       #associates new song instance with the artist from the filenames
       artist_name = file_name.split(" - ")[0]
       song_name = file_name.split(" - ")[1]
-      #song.artist_name = arist_name
       song = Song.new(song_name)
-
+      song.artist_name = arist_name
+      song
   end
 end
