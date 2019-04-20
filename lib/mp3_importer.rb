a84b9441774f76ced297b9c206041adbbd6812ec
Karen V <karenvo@usc.edu>
2019-04-20 10:42:22 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,7 +10,7 @@ class MP3Importer
     #files normalizes the filename to just the mp3 file name with no path
     Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
   end
-  def import(file)
+  def import
   #imports the files into the library by creating songs from a filename
     #list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
     #@files.each { |file| Song.new_by_filename(file) }
