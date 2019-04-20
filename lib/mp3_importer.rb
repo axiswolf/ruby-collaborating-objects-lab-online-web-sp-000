885a28faa9857185de1109d28961a0da5cc960bd
Karen V <karenvo@usc.edu>
2019-04-20 10:31:48 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -12,7 +12,7 @@ class MP3Importer
   end
   def import(list_of_filenames)
   #imports the files into the library by creating songs from a filename
-    #list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
     files.each { |file| Song.new_by_filename(file) }
   end
 end
