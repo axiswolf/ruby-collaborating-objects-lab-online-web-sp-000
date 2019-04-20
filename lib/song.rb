ade19cd5951dc182437886c947e4f463a2d790bd
Karen V <karenvo@usc.edu>
2019-04-20 10:48:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,9 +14,7 @@ class Song
     self.artist = artist
     artist.add_song(self)
   end
-  def songs
-    @songs
-  end
+
   def self.new_by_filename(file)
       #creates new instance of a song from the file that's passed
       #associates new song instance with the artist from the filenames
