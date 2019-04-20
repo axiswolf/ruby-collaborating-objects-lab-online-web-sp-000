adba32d54ab16c86d26309fe519d38452e5c1e10
Karen V <karenvo@usc.edu>
2019-04-20 10:48:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
   def self.new_by_filename(file)
       #creates new instance of a song from the file that's passed
       #associates new song instance with the artist from the filenames
-      artist = file.split(" - ")[0]
+      artist_name = file.split(" - ")[0]
       song_name = file.split(" - ")[1]
       song = Song.new(song_name)
       song.artist_name = artist_name
