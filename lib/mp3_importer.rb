13343179678bedca2ff1c9a821640b2c223ad0ad
Karen V <karenvo@usc.edu>
2019-04-20 10:34:14 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,7 +3,7 @@ class MP3Importer
   attr_reader :path, :file
   def initialize(path)
     @path = path
-  #  @files = Dir.entries(path).grep(/.*\.mp3/)
+    @files = Dir.entries(path).grep(/.*\.mp3/)
   end
   def files
     #files load all the mp3 files in the path directory
@@ -13,6 +13,7 @@ class MP3Importer
   def import(files)
   #imports the files into the library by creating songs from a filename
     #list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
-    @files.each { |file| Song.new_by_filename(file) }
+    #@files.each { |file| Song.new_by_filename(file) }
+    @files.each {|file| Song.new_by_filename(file)}
   end
 end
