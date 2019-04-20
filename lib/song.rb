725315bbe090912b22f42ff1645cd617e320366f
Karen V <karenvo@usc.edu>
2019-04-20 10:08:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,8 +19,16 @@ class Song
       self.artist.name = name
     end
   end
-  def self.new_by_filename
+  def self.new_by_filename(song_name)
       #creates new instance of a song from the file that's passed
       #associates new song instance with the artist from the filenames
+      new_song = self.all.find {|song| song.name == song_name}
+      if new_song
+         new_song
+       else
+         new_song =  Song.new(song_name)
+         new_song.save
+         new_song
+       end
   end
 end
