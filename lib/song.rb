9464ef8d64d193753cdcc44c6460e0eb10a314c6
Karen V <karenvo@usc.edu>
2019-04-20 10:29:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,6 @@ class Song
     #initialize with #name accepts a name for the Song
     @name = name
   end
-
   def artist_name=(name) # this method collaborates the Artist
     if (self.artist.nil?)
       self.artist = Artist.new(name)
@@ -12,11 +11,11 @@ class Song
       self.artist.name = name
     end
   end
-  def self.new_by_filename(file_name)
+  def self.new_by_filename(file)
       #creates new instance of a song from the file that's passed
       #associates new song instance with the artist from the filenames
-      artist_name = file_name.split(" - ")[0]
-      song_name = file_name.split(" - ")[1]
+      artist_name = file.split(" - ")[0]
+      song_name = file.split(" - ")[1]
       song = Song.new(song_name)
       song.artist_name = artist_name
       song
