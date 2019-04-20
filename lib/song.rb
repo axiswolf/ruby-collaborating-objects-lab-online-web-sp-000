264bf59095aba992ec8e94496d500fd447fd5a33
Karen V <karenvo@usc.edu>
2019-04-20 10:37:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
       artist_name = file.split(" - ")[0]
       song_name = file.split(" - ")[1]
       song = Song.new(song_name)
-      song.artist_name = name
+      song.name = artist_name
       song
   end
 end
